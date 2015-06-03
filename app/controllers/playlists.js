365f48699de17799c4dc2de0cae6f861d2edd165
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-06-03 16:34:48 +0200

    Don't know if this can be useful...

--- app/controllers/playlists.js
+++ app/controllers/playlists.js
@@ -1,9 +1,21 @@
 import Ember from "ember";
 
 export default Ember.ArrayController.extend({
-  content: function() {
+  needs: ['playlist'],
+  activePlaylist: Ember.computed.alias('controllers.playlist.content'),
+
+  all: function() {
     return this.get('store').all('playlist');
   }.property(),
+
+  content: function() {
+    var activePlaylistId = this.get('activePlaylist.id');
+
+    return this.get('all').map(function(playlist) {
+      playlist.set('isActive', playlist.get('id') == activePlaylistId);
+      return playlist;
+    }, this);
+  }.property('all', 'activePlaylist.id'),
 
   mine: Ember.computed.filterBy('content', 'isMine')
 });
