3e1461373926feaa54b494b6f6fad60764026b95
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-06-09 19:12:14 +0200

    Move actions to tracks list component

--- app/components/tracks-list.js
+++ app/components/tracks-list.js
@@ -3,16 +3,36 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['tracks-list-component'],
   tagName: "table",
-  //If the user has the track in some playlist
-  fromPlaylist: true,
+  action: "play",
+
+  fromPlaylist: true, //If the user has the track in some playlist
+  sortProperty: "addedAt",
+  sortAscending: false,
+
+  sortedContent: Ember.computed.sort('content', 'tracksSorting'),
+
+  tracksSorting: Ember.computed('sortProperty', 'sortAscending', {
+    get() {
+      var property = this.get('sortProperty');
+      var direction = this.get('sortAscending') ? 'asc' : 'desc';
+
+      return [`${property}:${direction}`];
+    }
+  }),
 
   actions: {
-    changeSort() {
+    changeSort(property) {
+      var currentSortProperty = this.get('sortProperty');
 
+      if (currentSortProperty === property) {
+        this.toggleProperty('sortAscending');
+      }
+
+      this.set('sortProperty', property);
     },
 
-    play() {
-      debugger;
+    play(track) {
+      this.sendAction('action', track);
     }
   }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   title: "Spotyness",
+  
   beforeModel: function() {
     return this.get('store').find('playlist');
+  },
+
+  actions: {
+    play(track) {
+      if (!track) return;
+
+      this.controllerFor('application').play(track);
+    }
   }
 });
