0b4baaaa7d7943b48c83ab638fafdf0ef5a8bd8e
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-09-07 15:04:14 +0200

    Add GA tracker

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.googleAnalytics = {
+      webPropertyId: 'UA-67282817-1'
+    };
   }
 
   return ENV;
