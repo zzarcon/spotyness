18d492bfc44959273350e710ce2c8d2f1e71badd
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-01-03 23:11:33 +0100

    Upgrade things to Ember 2.2.0

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,13 +4,29 @@ export default Ember.Route.extend({
   title: "Spotyness",
 
   beforeModel() {
-    if (this.get('session.isLogged')) {
-      var dependencies = [this.get('store').find('playlist'), this.get('store').find('playlist', {user_id: "spotify"}) ];
-
-      return Ember.RSVP.all(dependencies);
+    if (this.session.get('isReady')) {
+      //TODO: Do we need to do something here?
     } else {
-      this.transitionTo('login');
+      return this.startSession();
     }
+  },
+
+  startSession() {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      this.session.start().then(() => {
+        if (this.session.get('isLogged')) {
+          var dependencies = [this.store.findAll('playlist'), this.store.queryRecord('playlist', {
+            user_id: "spotify"
+          })];
+
+          Ember.RSVP.all(dependencies).then(resolve);
+        } else {
+          //TODO: Check what happen here
+          resolve();
+          this.transitionTo('login');
+        }
+      });
+    });
   },
 
   actions: {
